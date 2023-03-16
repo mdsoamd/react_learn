c53b008c7ae78b462add66774ce50d9038ec6746
Md Somad <mdsomad10@gmail.com>
2023-03-16 19:30:36 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,70 +1,103 @@
-import './App.css';
-import Header from './components/Header';
-import React,{useState} from 'react';
-
-
-
+import "./App.css";
+import Header from "./components/Header";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
-  
-  const [num,setNum] = useState(1);              //* <-- Array disturbing
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [data, setData] = useState([]);
 
 
-  function inc(n){
-    setNum(num +n);
+  const addData = () => {
+        setData([...data,{name,email}])
+        setName("")
+        setEmail('')
   }
 
-  function dec(n){
-    setNum(num - n);
+
+  const removeItem = (index) => {
+    const arr = data
+    arr.splice(index,1);
+    setData([...data])
+      
   }
-
-
-
-  // let login = false
   
+  
+
   return (
     <div className="App">
+      <Header />
 
+      <div className="from">
+        <Stack direction="row" spacing={2}>
 
-      {/* {                                //* <-- jsx ka andar if else condition use Karne Ka Tarika
-        (()=>{
+          <TextField
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+          />
+
+          <TextField 
+           value={email}
+           onChange={(event) => setEmail(event.target.value)}
+          id="outlined-basic" 
+          label="Email" 
+          variant="outlined" />
           
-            if(login){
-              return <h1 style={{color:"black"}}>Login</h1>
-            }else{
-              return <h1 style={{color:"black"}}>SignUp</h1>
-            }
+          <Button onClick={addData} variant="contained" color="success">
+            <AddIcon />
+          </Button>
 
-        })()
-      } */}
-      
-
-      <Header/>                             {/* <-- Header component call */}
+        </Stack>
+      </div>
 
 
-       <div className='main'>
+
+
+
+
+      {/* Data */}
+      <div className="data">
+         
+         <div className="data_val">
+
+           <h4>Name</h4>
+           <h4>Email</h4>
+           <h4>Remove</h4>
+          
+         </div>
+
+         {
+            data.map((element,index)=>{
+              return(
+                <div className="data_val">
+
+                <h4>{element.name}</h4>
+                <h4>{ element.email}</h4>
+                <Stack>
+                <Button onClick={()=>removeItem(index)} variant="contained" color="error">
+                   <DeleteIcon/>
+                  </Button>
+                  </Stack>
+               
+              </div>
+              )
+            })
+         }
         
-        <h1 className='heading'>{num}</h1>
+      </div>
 
-        <div className='buttons'>
-        <button className='btn' onClick={()=> inc(2)}>increment</button>
-        <button className='btn' onClick={()=> dec(1)}>decrement</button>
-        </div>
-        
-      
-        
-       </div>
-      
 
-      
-      
-      {/* <img src="/favicon.ico" alt="" /> */}
-      
       
     </div>
   );
 }
- 
+
 export default App;
-
-
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+const Header = () => {
+  return (
+    <div className="header">
+
+        <h1> Record keeping</h1>
+       
+        
+        </div>
+  )
+}
+
+export default Header
